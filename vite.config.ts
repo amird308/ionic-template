0c0a978388154038df3367883d18d80215523a98
damirchi <damirchi@pro.ai>
2023-06-06 23:08:58 -0700

    chore: set alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    legacy(),
   ],
+  resolve: {
+    alias: {
+      '@modules': path.resolve(__dirname, './src/modules'),
+      '@hooks': path.resolve(__dirname, './src/shared/hooks'),
+      '@models': path.resolve(__dirname, './src/shared/models'),
+      '@stores': path.resolve(__dirname, './src/shared/stores'),
+      '@helpers': path.resolve(__dirname, './src/shared/helpers'),
+      '@services': path.resolve(__dirname, './src/shared/services'),
+      '@constants': path.resolve(__dirname, './src/shared/constants'),
+      '@components': path.resolve(__dirname, './src/shared/components'),
+      '@infrastructure': path.resolve(__dirname, './src/infrastructure'),
+      '@entities': path.resolve(__dirname, './src/infrastructure/entities')
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
