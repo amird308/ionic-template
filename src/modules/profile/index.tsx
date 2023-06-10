8b09cb8fd9a686e2a6e9fea1db70c76d8ab5b3e6
damirchi <damirchi@pro.ai>
2023-06-10 15:05:04 -0700

    feat: change page's css to scss

--- src/modules/profile/index.tsx
+++ src/modules/profile/index.tsx
@@ -1,9 +1,9 @@
 import { IonHeader, IonPage, IonContent, IonFooter, IonToolbar, IonTitle, IonButtons, IonBackButton } from '@ionic/react';
-import './style.css';
+import './style.scss';
 
 const Profile: React.FC = () => {
   return (
-    <IonPage>
+    <IonPage id="profile-page">
       <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
