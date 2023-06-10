96ab296bd2d64802d1f7f116836e1355470f1438
damirchi <damirchi@pro.ai>
2023-06-10 15:05:34 -0700

    feat: overwrite button and test it in home

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,13 +1,18 @@
 import { IonHeader, IonPage, IonContent, IonFooter, IonToolbar, IonTitle, IonButtons, IonBackButton, IonButton, useIonRouter } from '@ionic/react';
-import './style.css';
 import LINKS from '../../shared/constants/links';
 import useTheme from '@hooks/useTheme';
+import Button, { IButtonRef } from '@components/button';
+
+import './style.scss';
+import { useRef } from 'react';
 
 const Home: React.FC = () => {
   const router = useIonRouter();
   const theme = useTheme();
+
+  const ref = useRef<IButtonRef | null>(null);
   return (
-    <IonPage>
+    <IonPage id="home-page">
       <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
@@ -25,8 +30,16 @@ const Home: React.FC = () => {
         <IonButton onClick={()=> {
           theme.toggleTheme();
         }}>
-          navigate
+          theme
         </IonButton>
+        <IonButton  onClick={ ()=> ref?.current?.click()}>
+          theme
+        </IonButton>
+        <Button ref={ref} onClick={()=> {
+          console.log("hi man");
+        }}>
+          hi  
+        </Button>
       </IonContent>
       <IonFooter>
 
---
+++ src/shared/components/button/index.tsx
@@ -0,0 +1,23 @@
+
+
+import { ForwardRefExoticComponent, HTMLAttributes, RefAttributes, forwardRef } from "react";
+import { IonButton, RouterDirection, RouterOptions, AnimationBuilder } from "@ionic/react";
+import type { JSX } from '@ionic/core/components';
+import { IonicReactProps } from "@ionic/react/dist/types/components/IonicReactProps";
+import "./style.css";
+import React from "react";
+
+export type IButtonProps = Omit<JSX.IonButton, "routerDirection"> & Omit<HTMLAttributes<HTMLIonButtonElement>, "style"> & IonicReactProps & RefAttributes<HTMLIonButtonElement> & {
+        routerLink?: string | undefined;
+        routerDirection?: RouterDirection | undefined;
+        routerOptions?: RouterOptions | undefined;
+        routerAnimation?: AnimationBuilder | undefined;
+};
+
+export type IButtonRef = HTMLIonButtonElement;
+
+const Button = forwardRef<IButtonRef, IButtonProps>((props, ref)=> {
+    return <IonButton {...props} ref={ref}> {props.children} </IonButton>
+})
+
+export default Button;
