d2a454ecfc602be079c1add8ae179a1784b13e38
damirchi <damirchi@pro.ai>
2023-08-21 14:45:05 -0700

    chore: update pwa configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,85 +3,89 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import legacy from '@vitejs/plugin-legacy';
-import replace from '@rollup/plugin-replace';
-import type { ManifestOptions, VitePWAOptions } from 'vite-plugin-pwa';
+import type { VitePWAOptions } from 'vite-plugin-pwa';
 
 
 const pwaOptions: Partial<VitePWAOptions> = {
-  mode: 'development',
+  mode: 'development', 
+  registerType: "autoUpdate",
+  devOptions: {
+    enabled: true
+  },
   base: '/',
   includeAssets: ['favicon.svg'],
   manifest: {
-    name: 'PWA Router',
-    short_name: 'PWA Router',
+    name: 'ionic template',
+    short_name: 'ionic template',
     description: 'My Awesome App description',
     theme_color: '#ffffff',
-    start_url: "https://localhost:5728",
     display: "fullscreen",
     icons: [
       {
-        src: 'icon-192.webp', // <== don't add slash, for testing
-        sizes: '192x192',
-        type: 'image/webp',
+        "src": "/icons/icon-48x48.png",
+        "sizes": "48x48",
+        "type": "image/png",
+        "purpose": "maskable any"
       },
       {
-        src: '/icon-512.webp', // <== don't remove slash, for testing
-        sizes: '512x512',
-        type: 'image/webp',
+        "src": "/icons/icon-72x72.png",
+        "sizes": "72x72",
+        "type": "image/png",
+        "purpose": "maskable any"
       },
       {
-        src: '/icon-512.webp', // <== don't add slash, for testing
-        sizes: '512x512',
-        type: 'image/webp',
-        purpose: 'any maskable',
+        "src": "/icons/icon-96x96.png",
+        "sizes": "96x96",
+        "type": "image/png",
+        "purpose": "maskable any"
       },
+      {
+        "src": "/icons/icon-128x128.png",
+        "sizes": "128x128",
+        "type": "image/png",
+        "purpose": "maskable any"
+      },
+      {
+        "src": "/icons/icon-144x144.png",
+        "sizes": "144x144",
+        "type": "image/png",
+        "purpose": "maskable any"
+      },
+      {
+        "src": "/icons/icon-152x152.png",
+        "sizes": "152x152",
+        "type": "image/png",
+        "purpose": "maskable any"
+      },
+      {
+        "src": "/icons/icon-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png",
+        "purpose": "maskable any"
+      },
+      {
+        "src": "/icons/icon-384x384.png",
+        "sizes": "384x384",
+        "type": "image/png",
+        "purpose": "maskable any"
+      },
+      {
+        "src": "/icons/icon-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png",
+        "purpose": "maskable any"
+      }
     ],
   },
-  devOptions: {
-    enabled: true,
-    /* when using generateSW the PWA plugin will switch to classic */
-    type: 'module',
-    navigateFallback: 'index.html',
-  },
-  workbox: {
-    clientsClaim: true,
-    skipWaiting: true
-  },
 }
-
-const replaceOptions = { __DATE__: new Date().toISOString(), preventAssignment: true }
-const claims = true;
-const reload = true;
-const selfDestroying = true;
-const sw = true;
-if (sw) {
-  pwaOptions.srcDir = 'src';
-  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts';
-  pwaOptions.strategies = 'injectManifest';
-  (pwaOptions.manifest as Partial<ManifestOptions>).name = 'PWA Inject Manifest';
-  (pwaOptions.manifest as Partial<ManifestOptions>).short_name = 'PWA Inject';
-}
-
-if (claims)
-  pwaOptions.registerType = 'autoUpdate';
-
-if (reload) {
-  // @ts-expect-error just ignore
-  replaceOptions.__RELOAD_SW__ = 'true';
-}
-
-if (selfDestroying)
-  pwaOptions.selfDestroying = selfDestroying;
 
 export default defineConfig({
-  // base: process.env.BASE_URL || 'https://github.com/',
   build: {
     sourcemap: true,
   },
   plugins: [
     react(),
     VitePWA(pwaOptions),
-    // replace(replaceOptions),
     legacy(),
   ],
   resolve: {
