13e6b368c83717525f1189e3e843a20a061a18b6
damirchi <damirchi@pro.ai>
2023-09-12 16:20:25 -0700

    feat: implement profile info user section

--- src/modules/profile/index.tsx
+++ src/modules/profile/index.tsx
@@ -1,6 +1,9 @@
-import { IonHeader, IonPage, IonContent, IonFooter, IonToolbar, IonTitle, IonButtons, IonBackButton } from '@ionic/react';
-import './style.scss';
+import { IonHeader, IonPage, IonContent, IonFooter, IonToolbar, IonTitle, IonButtons, IonBackButton, IonImg } from '@ionic/react';
 import { useGetProfileQuery } from '@services/profile';
+import Card from '@components/card';
+
+import './style.scss';
+import Button from '@components/button';
 
 const Profile: React.FC = () => {
   const { data } = useGetProfileQuery(undefined, {});
@@ -15,8 +18,35 @@ const Profile: React.FC = () => {
           <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen={true}>
-
+      <IonContent>
+      <Card className='p-4'>
+        <div className='flex items-center gap-5'>
+          <IonImg
+            className='imageProfile'
+            src="https://docs-demo.ionic.io/assets/madison.jpg"
+            alt="The Wisconsin State Capitol building in Madison, WI at night"
+          />
+          <div className='flex-1 flex flex-col'>
+            <div>
+              profile 
+            </div>
+            <div>
+              profile 
+            </div>
+          </div>
+        </div>
+        <Button color='primary' fill='outline' className='mt-10 w-full'>
+          edit profile
+        </Button>
+      </Card>
+      <Card className='p-4 mt-4 flex flex-col'>
+        <div className='text-gray-50'>
+            description 
+        </div>
+        <div>
+            my name is amir, please give me feedback. 
+        </div>
+      </Card>
       </IonContent>
       <IonFooter>
 
